f938393374d5491696090e1218e1b4ddc1a94a08
aarneharju <35223549+aarneharju@users.noreply.github.com>
2022-12-13 18:01:30 +0200

    Now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,25 +1,40 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-# The user has heard about a new online todo list and goes to check out its homepage
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# The user notices the page title and header mention todo list
-assert 'Todo' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# The user is invited to enter a todo item straight away
+    def tearDown(self):
+        self.browser.quit()
 
-# The user types "Fill up the dish machine" into a text box
+    def test_can_start_a_list_and_retrieve_it_later(self):
 
-# When the user hits enter, the page updates, and now the page lists "1 - Fill up the dish machine" as an item in a todo list
+        # The user has heard about a new online todo list and goes to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-# There is still a text box inviting the user to add another item. The user enters "Cook dinner"
+        # The user notices the page title and header mention todo list
+        self.assertIn('Todo', self.browser.title)
+        self.fail('Finish the test!')
 
-# The page updates again and shows both items on the list
+        # The user is invited to enter a todo item straight away
 
-# The user wonders whether the site will remember her list. Then the user sees that the site has generated a unique URL -- there is some explanatory text to that effect
+        # The user types "Fill up the dish machine" into a text box
 
-# The user visits that URL - the todo list is still there
+        # When the user hits enter, the page updates, and now the page lists "1 - Fill up the dish machine" as an item in a todo list
 
-browser.quit()
+        # There is still a text box inviting the user to add another item. The user enters "Cook dinner"
+
+        # The page updates again and shows both items on the list
+
+        # The user wonders whether the site will remember her list. Then the user sees that the site has generated a unique URL -- there is some explanatory text to that effect
+
+        # The user visits that URL - the todo list is still there
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# browser.quit()

9e2599bf90c2d259796c88e31b0718ba4aef0e9d
aarneharju <35223549+aarneharju@users.noreply.github.com>
2022-12-16 12:22:36 +0200

    Changed explicit wait of 1s to a loopthat polls our app

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -38,8 +49,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # When the user hits enter, the page updates, and now the page lists "1: Fill up the dish machine" as an item in a todo list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Fill up the dish machine')
+        self.wait_for_row_in_list_table('1: Fill up the dish machine')
 
         # self.assertTrue(
         #     any(row.text == '1: Fill up the dish machine' for row in rows),
@@ -50,11 +60,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Cook dinner')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again and shows both items on the list
-        self.check_for_row_in_list_table('1: Fill up the dish machine')
-        self.check_for_row_in_list_table('2: Cook dinner')
+        self.wait_for_row_in_list_table('1: Fill up the dish machine')
+        self.wait_for_row_in_list_table('2: Cook dinner')
 
         # The user wonders whether the site will remember her list. Then the user sees that the site has generated a unique URL -- there is some explanatory text to that effect
 

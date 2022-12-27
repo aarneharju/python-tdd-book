47514bd40f5320bf5c6eb8981a055206017afcad
aarneharju <35223549+aarneharju@users.noreply.github.com>
2022-12-27 13:41:11 +0200

    Fixed functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
 
         # The user has heard about a new online todo list and goes to check out its homepage
         self.browser.get(self.live_server_url)
@@ -41,8 +41,10 @@ class NewVisitorTest(LiveServerTestCase):
 
         # The user is invited to enter a todo item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute(
-            'placeholder'), 'Enter a todo item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a todo item'
+        )
 
         # The user types "Fill up the dish machine" into a text box
         inputbox.send_keys('Fill up the dish machine')
@@ -62,17 +64,19 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again and shows both items on the list
-        self.wait_for_row_in_list_table('1: Fill up the dish machine')
         self.wait_for_row_in_list_table('2: Cook dinner')
+        self.wait_for_row_in_list_table('1: Fill up the dish machine')
 
-        # The user wonders whether the site will remember her list. Then the user sees that the site has generated a unique URL -- there is some explanatory text to that effect
-        def test_multiple_users_can_start_lists_at_different_urls(self):
-            # The user starts a new todo list
-            self.browser.get(self.live_server_url)
-            inputbox = self.browser.find_element_by_id('id_new_item')
-            inputbox.send_keys('Buy nuts')
-            inputbox.send_keys('Keys.ENTER')
-            self.wait_for_row_in_list_table('1: Buy nuts')
+
+    # The user wonders whether the site will remember her list. Then the user sees that the site has generated a unique URL -- there is some explanatory text to that effect
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+
+        # The user starts a new todo list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy nuts')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy nuts')
 
         # The user notices that the list has a unique URL
         user1_list_url = self.browser.current_url
@@ -88,7 +92,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('dish', page_text)
-        self.assertNotIn('cook', page_text)
+        self.assertNotIn('dinner', page_text)
 
         # User2 starts a new list by entering new item. He is less interesting than user1
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -102,6 +106,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(user2_list_url, user1_list_url)
 
         # Again, there is no trace of user1's list
-        page_text = self.browser.find_element_by_tagname('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('dish', page_text)
         self.assertIn('Buy milk', page_text)
